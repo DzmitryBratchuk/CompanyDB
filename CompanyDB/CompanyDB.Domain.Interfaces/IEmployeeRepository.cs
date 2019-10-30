c524348479be3e8b8ceacea953d4d66f1042a52e
DzmitryBratchuk <bdimmersion@gmail.com>
2019-10-30 09:09:42 -0700

    Add CompanyDB.Domain.Interfaces

---
+++ CompanyDB/CompanyDB.Domain.Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,17 @@
+﻿using CompanyDB.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CompanyDB.Domain.Interfaces
+{
+    public interface IEmployeeRepository : IDisposable
+    {
+        IQueryable<Employee> GetAllEmployees();
+        Employee GetEmployee(object id);
+        void Create(Employee employee, bool shouldSaveChanges = true);
+        void Update(Employee employee, bool shouldSaveChanges = true);
+        void Delete(object id, bool shouldSaveChanges = true);
+        int SaveChanges();
+    }
+}
