f2f367d1de393cbb5e5f1bd736edfc1ca298ed7a
DzmitryBratchuk <bdimmersion@gmail.com>
2019-11-01 10:49:29 -0700

    Change IRepository to generic

---
+++ CompanyDB/CompanyDB.Domain.Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using CompanyDB.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CompanyDB.Domain.Interfaces
+{
+    public interface IRepository<TEntity> : IDisposable
+        where TEntity : class
+    {
+        IQueryable<TEntity> GetAll();
+        TEntity GetById(object id);
+        void Create(TEntity entity, bool shouldSaveChanges = true);
+        void Update(TEntity entity, bool shouldSaveChanges = true);
+        void Delete(object id, bool shouldSaveChanges = true);
+        int SaveChanges();
+    }
+}
