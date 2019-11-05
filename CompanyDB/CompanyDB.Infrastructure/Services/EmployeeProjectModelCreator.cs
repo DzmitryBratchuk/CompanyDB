3af346172a4bf86e72a45a8c170145a6dce03f49
DzmitryBratchuk <bdimmersion@gmail.com>
2019-11-05 06:44:44 -0800

    Implement services

--- CompanyDB/CompanyDB.Infrastructure/Services/EmployeeProjectModelCreator.cs
+++ CompanyDB/CompanyDB.Infrastructure/Services/EmployeeProjectModelCreator.cs
@@ -1,16 +1,42 @@
-﻿using CompanyDB.Core.Interfaces.Services;
+﻿using CompanyDB.Core.Entities;
+using CompanyDB.Core.Interfaces.Data;
+using CompanyDB.Core.Interfaces.Services;
 using CompanyDB.Core.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CompanyDB.Infrastructure.Services
 {
     class EmployeeProjectModelCreator : IModelCreator<EmployeeProjectViewModel>
     {
+        private IRepository<Employee> _employeeRepository;
+        private IRepository<EmployeeProject> _employeeProjectRepository;
+        private IRepository<Project> _projectRepository;
+
+
+        public EmployeeProjectModelCreator(IRepository<Employee> employeeRepository,
+                                           IRepository<EmployeeProject> employeeProjectRepository,
+                                           IRepository<Project> projectRepository)
+        {
+            _employeeRepository = employeeRepository;
+            _employeeProjectRepository = employeeProjectRepository;
+            _projectRepository = projectRepository;
+        }
+
         public IEnumerable<EmployeeProjectViewModel> GetAll()
         {
-            throw new NotImplementedException();
+            return from e in _employeeRepository.GetAll()
+                   join ep in _employeeProjectRepository.GetAll() on e.Id equals ep.EmployeeId
+                   join p in _projectRepository.GetAll() on ep.ProjectId equals p.Id
+                   select new EmployeeProjectViewModel
+                   {
+                       Name = e.FirstName,
+                       Surname = e.LastName,
+                       ProjectTitle = p.ProjectName
+                   };
         }
     }
 }
--- CompanyDB/CompanyDB.Infrastructure/Services/ProjectModelCreator.cs
+++ CompanyDB/CompanyDB.Infrastructure/Services/ProjectModelCreator.cs
@@ -1,16 +1,27 @@
-﻿using CompanyDB.Core.Interfaces.Services;
+﻿using CompanyDB.Core.Entities;
+using CompanyDB.Core.Interfaces.Data;
+using CompanyDB.Core.Interfaces.Services;
 using CompanyDB.Core.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CompanyDB.Infrastructure.Services
 {
     class ProjectModelCreator : IModelCreator<ProjectViewModel>
     {
+        private IRepository<Project> _projectRepository;
+
+        public ProjectModelCreator(IRepository<Project> repository)
+        {
+            _projectRepository = repository;
+        }
+
         public IEnumerable<ProjectViewModel> GetAll()
         {
-            throw new NotImplementedException();
+            return _projectRepository.GetAll()
+                .Select(p => new ProjectViewModel { ProjectTitle = p.ProjectName });
         }
     }
 }
