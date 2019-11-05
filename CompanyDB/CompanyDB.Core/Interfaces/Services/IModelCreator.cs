2e8155a041625c1bdc9f1407bbd0e09aa9cd3dc0
DzmitryBratchuk <bdimmersion@gmail.com>
2019-11-05 01:38:51 -0800

    Add Services layer

---
+++ CompanyDB/CompanyDB.Core/Interfaces/Services/IModelCreator.cs
@@ -0,0 +1,13 @@
+﻿using CompanyDB.Core.Interfaces.SharedKernel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyDB.Core.Interfaces.Services
+{
+    public interface IModelCreator<TModel> 
+        where TModel : class, IModel
+    {
+        IEnumerable<TModel> GetAll();
+    }
+}
---
+++ CompanyDB/CompanyDB.Infrastructure/Services/ModelCreator.cs
@@ -0,0 +1,17 @@
+﻿using CompanyDB.Core.Interfaces.Services;
+using CompanyDB.Core.Interfaces.SharedKernel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyDB.Infrastructure.Services
+{
+    public class ModelCreator<TModel> : IModelCreator<TModel>
+        where TModel : class, IModel
+    {
+        public IEnumerable<TModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
